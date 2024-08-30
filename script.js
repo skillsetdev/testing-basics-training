7f8a2394afe0b1bbf51f56cc2850d684e145ae6d
skillsetdev <skillsdev3@gmail.com>
2024-08-30 23:16:15 +0200

    Finish practicing TDD according to the curricullum

--- script.js
+++ script.js
@@ -1,3 +1,79 @@
-export default function sum(a, b) {
+export function sum(a, b) {
   return a + b;
+}
+export function capitalize(input) {
+  const charArray = input.split("");
+  charArray[0] = charArray[0].toUpperCase();
+  const output = charArray.join("");
+  return output;
+}
+export function reverseString(input) {
+  const charArray = input.split("");
+  return charArray.reverse().join("");
+}
+export class Calculator {
+  add(a, b) {
+    return a + b;
+  }
+  subtract(a, b) {
+    return a - b;
+  }
+  divide(a, b) {
+    return a / b;
+  }
+  multiply(a, b) {
+    return a * b;
+  }
+}
+export function caesarCipher(string, shiftFactor) {
+  const charArray = string.split("");
+  return charArray
+    .map((character) => shiftCharacter(character, shiftFactor))
+    .join("");
+}
+function shiftCharacter(character, shiftFactor) {
+  const alphabetString = "abcdefghijklmnopqrstuvwxyz";
+  const alphabetArr = alphabetString.split("");
+  const upperCaseAlphabetArr = alphabetString.toUpperCase().split("");
+  const currentIndex = isUpperCase(character)
+    ? upperCaseAlphabetArr.findIndex((value) => value === character)
+    : alphabetArr.findIndex((value) => value === character);
+  if (currentIndex === -1) return character;
+  let newIndex = (currentIndex + shiftFactor) % 26; //modulo operation excludes out-of-boundary search
+
+  return isUpperCase(character)
+    ? upperCaseAlphabetArr[newIndex]
+    : alphabetArr[newIndex];
+}
+function isUpperCase(character) {
+  return character === character.toUpperCase();
+}
+export function analyzeArray(array) {
+  const sum = array.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+  const average = Math.floor(sum / array.length);
+  const length = array.length;
+  const sortedArr = bubbleSort(array);
+  const min = sortedArr[0];
+  const max = sortedArr[sortedArr.length - 1];
+  return {
+    average: average,
+    min: min,
+    max: max,
+    length: length,
+  };
+}
+export function bubbleSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        let tmp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = tmp;
+      }
+    }
+  }
+  return array;
 }
--- script.test.js
+++ script.test.js
@@ -1,5 +1,42 @@
-import sum from "./script.js";
+import {
+  sum,
+  capitalize,
+  reverseString,
+  Calculator,
+  caesarCipher,
+  bubbleSort,
+  analyzeArray,
+} from "./script.js";
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
+});
+test('turns "string" to "String"', () => {
+  expect(capitalize("string")).toMatch("String");
+});
+test("reverses the given string", () => {
+  expect(reverseString("string")).toBe("gnirts");
+});
+test("adds, subtracts, divides and multiplies", () => {
+  const calculator = new Calculator();
+  expect(calculator.add(3, 4)).toBe(7);
+  expect(calculator.subtract(3, 4)).toBe(-1);
+  expect(calculator.divide(3, 4)).toBeCloseTo(0.75);
+  expect(calculator.multiply(3, 4)).toBe(12);
+});
+test("caesar-ciphers a string", () => {
+  expect(caesarCipher("xyz", 3)).toBe("abc");
+  expect(caesarCipher("HeLLo", 29)).toBe("KhOOr");
+  expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
+});
+test("bubblesorts", () => {
+  expect(bubbleSort([1, 8, 3, 4, 2, 6])).toEqual([1, 2, 3, 4, 6, 8]);
+});
+test("analyzes an array", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
 });
